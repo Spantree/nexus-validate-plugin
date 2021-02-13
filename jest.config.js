ffdb4de3c2b2e514a37e00df02725c15746f767d
Ahmed Elywa <ahmed.elywa@icloud.com>
2021-02-14 00:35:27 +0200

    remove unused files

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   clearMocks: true,
   testEnvironment: 'node',
-  collectCoverage: true,
-  coverageDirectory: './coverage',
-  collectCoverageFrom: ['src/**/*.ts'],
   verbose: true,
   globals: {
     'ts-jest': {
